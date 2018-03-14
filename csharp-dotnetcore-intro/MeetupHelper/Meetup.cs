19b2acb7ceb1d52e9298c078c620a1c466014dfb
larrynburris <larry.n.burris@gmail.com>
2018-03-13 22:15:23 -0400

    Add meetup class and implement simple greetings

---
+++ csharp-dotnetcore-intro/MeetupHelper/Meetup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeetupHelper
+{
+    public class Meetup
+    {
+        public List<string> Organizers { get; set; }
+        public List<string> Attendees { get; set; }
+        public string Name { get; set; }
+
+        public Meetup()
+        {
+            Organizers = new List<string>();
+            Attendees = new List<string>();
+        }
+
+        public void Start()
+        {
+            foreach(var organizer in Organizers)
+            {
+                foreach(var attendee in Attendees)
+                {
+                    Console.WriteLine($"{organizer} greets {attendee} to {Name}");
+                }
+            }
+        }
+
+        public void End()
+        {
+            foreach (var organizer in Organizers)
+            {
+                foreach (var attendee in Attendees)
+                {
+                    Console.WriteLine($"{organizer} thanks {attendee} for coming and welcomes them back to the next {Name} meetup.");
+                }
+            }
+        }
+    }
+}
--- csharp-dotnetcore-intro/MeetupHelper/Program.cs
+++ csharp-dotnetcore-intro/MeetupHelper/Program.cs
@@ -6,7 +6,32 @@ namespace MeetupHelper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var meetup = new Meetup();
+
+            meetup.Name = "Operation Code";
+
+            meetup.Organizers.Add("James (O)");
+            meetup.Organizers.Add("Shirley (O)");
+
+            meetup.Attendees.Add("Mary (A)");
+            meetup.Attendees.Add("Ben (A)");
+            meetup.Attendees.Add("Jack (A)");
+            meetup.Attendees.Add("Debra (A)");
+            meetup.Attendees.Add("Antonio (A)");
+            meetup.Attendees.Add("Evette (A)");
+            meetup.Attendees.Add("William (A)");
+            meetup.Attendees.Add("Cheryl (A)");
+            meetup.Attendees.Add("Levi (A)");
+
+            meetup.Start();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Meetup in progress. Fun stuff is happening and people are learning. Press enter to continue...");
+            Console.ReadLine();
+
+            meetup.End();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Meetup has ended. Let's go home. Press enter to continue...");
+            Console.ReadLine();
         }
     }
 }
