1a7a98ca5a52a149494e57e14433f9eb76103b8f
larrynburris <larry.n.burris@gmail.com>
2018-03-13 21:59:33 -0400

    Create MeetupHelper project

---
+++ csharp-dotnetcore-intro/MeetupHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeetupHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
