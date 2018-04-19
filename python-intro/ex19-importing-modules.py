73335db7695d3c385c87e2f6ed6c044e3c78002e
larrynburris <larry.n.burris@gmail.com>
2018-04-19 12:15:34 -0400

    importing modules

---
+++ python-intro/ex19-importing-modules.py
@@ -0,0 +1,6 @@
+# same function from ex18-functions.py
+from ex19printfunctions import helloworld
+
+helloworld("Operation Code")
+
+helloworld("1701")
---
+++ python-intro/ex19printfunctions.py
@@ -0,0 +1,5 @@
+# same function from ex18-functions.py
+
+def helloworld(name):
+    greeting = "Hello, " + name
+    print(greeting)
