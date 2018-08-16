b7c344c82e1188c74043e3991aa0110e99a86594
Larry Burris III <larry.n.burris@gmail.com>
2018-08-15 23:10:00 -0400

    find and print timestamp strings with regex

--- python-log-parsing-example/parse.py
+++ python-log-parsing-example/parse.py
@@ -2,12 +2,15 @@ import os
 import re
 
 # Regex used to match a timestamp with format: yyyy-MM-dd HH:mm:ss,fff
-line_regex = re.compile(r'[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}')
+time_stamp_regex = re.compile(r'[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}')
 matches = 0
 log_file = os.path.join('C:', os.sep, 'AppointmentSystem', 'Logs', 'TRSServiceLog20180601')
 with open(log_file, "r") as in_file:
     for line in in_file:
-        if (line_regex.search(line)):
+        datetime_found = time_stamp_regex.match(line)
+        if datetime_found:
+            log_entry_timestamp = datetime_found.group()
+            print(log_entry_timestamp)
+        if (time_stamp_regex.search(line)):
             matches += 1
-            print(line)
 print(matches)
