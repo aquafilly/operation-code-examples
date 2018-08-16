539819a3c43fbbb8fc0205f4a14994866f38f1dd
Larry Burris III <larry.n.burris@gmail.com>
2018-08-16 00:03:33 -0400

    datetime is always at start of line

--- python-log-parsing-example/parse.py
+++ python-log-parsing-example/parse.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Regex used to match a timestamp with format: yyyy-MM-dd HH:mm:ss,fff
-time_stamp_regex = re.compile(r'[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}')
+time_stamp_regex = re.compile(r'^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}')
 matches = 0
 log_file = os.path.join('C:', os.sep, 'AppointmentSystem', 'Logs', 'TRSServiceLog20180601')
 with open(log_file, "r") as in_file:
